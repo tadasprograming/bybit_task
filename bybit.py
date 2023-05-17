a5eeea1d9ccd0db2de53f613c0ac30b13bce6b62
Tadas Vasiliauskas <tadasprivatus@gmail.com>
2023-05-17 14:28:03 +0300

    Sukurta Kline class

--- bybit.py
+++ bybit.py
@@ -1,5 +1,9 @@
 from pybit.unified_trading import HTTP
 import time
+from sqlalchemy import Column, Integer, BigInteger, Float
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
 
 API_KEY = "EKim1rHsFKKuAKybhe"
 API_SECRET = "XhEbHQpwNpUpoDIwFdG3YAzRdrzvp5oyBhob"
@@ -22,3 +26,22 @@ kline_dic = session.get_kline(
 )  # paskutinės paros data
 
 print(len(kline_dic["result"]["list"]))
+
+kline_engine = create_engine('sqlite:///kline.db', echo=True)
+Base = declarative_base()
+
+
+class Kline(Base):
+    __tablename__ = 'kline.db'
+   
+    id = Column(Integer, primary_key=True)
+    start_time = Column(BigInteger)
+    open_price = Column(Float)
+    high_price = Column(Float)
+    low_price = Column(Float)
+    close_price = Column(Float)
+    volume = Column(Float)
+    turnover = Column(Float)
+
+
+Base.metadata.create_all(kline_engine)
